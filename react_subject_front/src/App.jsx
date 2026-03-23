c73db12453e450124d7e1d34ca389f885d520713
JavaSinged <shinjiwoong1656@gmail.com>
2026-03-23 14:20:08 +0900

    Initial commit

---
+++ react_subject_front/src/App.jsx
@@ -0,0 +1,15 @@
+import styles from './App.module.css';
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className={styles.header}>
+        <h1>강의 목록</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
